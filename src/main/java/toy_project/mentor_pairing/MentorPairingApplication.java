6bea0dcd8545f124d179a6034e48035d5e71c618
yong <yonggyun0120@gmail.com>
2026-08-14 17:03:42 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/toy_project/mentor_pairing/MentorPairingApplication.java
@@ -0,0 +1,13 @@
+package toy_project.mentor_pairing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorPairingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorPairingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy_project/mentor_pairing/MentorPairingApplicationTests.java
@@ -0,0 +1,13 @@
+package toy_project.mentor_pairing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorPairingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
