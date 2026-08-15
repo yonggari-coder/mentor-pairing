6b0dfb47e41504f4fd23a13a951aade3f7e500c2
yong <yonggyun0120@gmail.com>
2026-08-15 19:35:59 +0900

    refactor(score-report): ScoreReport가 SubjectScore를 임베딩하도록 변경

--- src/main/java/domain/ScoreReport.java
+++ src/main/java/domain/ScoreReport.java
@@ -1,7 +1,6 @@
 package domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +8,30 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ScoreReport {
+
     @Id
     private Long reportId;
 
-    private Long studentId;
-    private int year;
-    private int semester;
+    @OneToOne(fetch= FetchType.LAZY)
+    private Student student;
+
+    @Embedded
+    @AttributeOverride(name="score", column=@Column(name="korean_score"))
+    private SubjectScore korean;
+
+    @Embedded
+    @AttributeOverride(name="score", column=@Column(name="english_score"))
+    private SubjectScore english;
+
+    @Embedded
+    @AttributeOverride(name="score", column=@Column(name="math_score"))
+    private SubjectScore math;
+
+    public int sumScore(){
+        int sum = 0;
+        sum+=korean.getScore();
+        sum+=english.getScore();
+        sum+=math.getScore();
+        return sum;
+    }
 }
