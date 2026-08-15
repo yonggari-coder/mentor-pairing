60a238f8177ec283c3f276b065bd391bfb0adeb4
yong <yonggyun0120@gmail.com>
2026-08-15 19:36:46 +0900

    refactor(application-status): 신청 상태 분리 고도화

--- src/main/java/domain/ApplicationStatus.java
+++ src/main/java/domain/ApplicationStatus.java
@@ -1,5 +1,8 @@
 package domain;
 
 public enum ApplicationStatus {
-    ACTIVE, CANCELED
+    APPROVED,   //관리자가 승인한 상태
+    PENDING,    //승인 대기 상태
+    CANCELED,   //신청자가 취소한 상태
+    DISAPPROVAL //관리자가 거절한 상태
 }
