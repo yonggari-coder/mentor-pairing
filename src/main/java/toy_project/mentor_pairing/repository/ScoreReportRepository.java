4802ac025500d35247d275fb047bbf9c2b63bbe9
yong <yonggyun0120@gmail.com>
2026-08-18 13:13:24 +0900

    feat: scoreReportRepository 생성

---
+++ src/main/java/toy_project/mentor_pairing/repository/ScoreReportRepository.java
@@ -0,0 +1,31 @@
+package toy_project.mentor_pairing.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import toy_project.mentor_pairing.domain.ScoreReport;
+
+@Repository
+@RequiredArgsConstructor
+public class ScoreReportRepository {
+
+    private final EntityManager em;
+
+    public void save(ScoreReport scoreReport){
+        em.persist(scoreReport);
+    }
+
+    public ScoreReport findById(Long id){
+        ScoreReport scoreReport = em.find(ScoreReport.class, id);
+        return scoreReport;
+    }
+
+    public ScoreReport findByStudentId(Long studentId){ //JPQL 사용하기.
+        return em.createQuery(
+                "select sr from ScoreReport sr where sr.student.studentId = :studentId",
+                        ScoreReport.class
+                )
+                .setParameter("studentId", studentId)
+                .getSingleResult();
+    }
+}
