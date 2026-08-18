f94732b280142b9a5418ea8f49e9e6db67ae0cf6
yong <yonggyun0120@gmail.com>
2026-08-18 23:46:59 +0900

    chore: 테스트 이름을 MentoringApplicationRepositoryTest로 변경

--- src/test/java/toy_project/mentor_pairing/repository/MentoringApplicationRepositoryTest.java
+++ src/test/java/toy_project/mentor_pairing/repository/MentoringApplicationRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @SpringBootTest
 @Transactional
-class MentoringApplicationTest {
+class MentoringApplicationRepositoryTest {
 
     @Autowired private EntityManager em;
     @Autowired private MentoringApplicationRepository mentoringApplicationRepository;
