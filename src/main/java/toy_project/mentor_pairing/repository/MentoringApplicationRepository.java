beda1ef8c22b1100c9aabfeb557af5e406634f49
yong <yonggyun0120@gmail.com>
2026-08-18 16:03:42 +0900

    feat: MentoringApplicationRepository 생성

---
+++ src/main/java/toy_project/mentor_pairing/repository/MentoringApplicationRepository.java
@@ -0,0 +1,34 @@
+package toy_project.mentor_pairing.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import toy_project.mentor_pairing.domain.MentoringApplication;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MentoringApplicationRepository {
+
+    private final EntityManager em;
+
+    public void save(MentoringApplication mentoringApplication){ //멘토링 신청
+        em.persist(mentoringApplication);
+    }
+
+    public List<MentoringApplication> findByStudentName(String name){
+        return em.createQuery("select ma from MentoringApplication ma where ma.applicant.name = :name",
+                        MentoringApplication.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+    public List<MentoringApplication> findByStudentId(Long id) {
+        return em.createQuery("select ma from MentoringApplication ma where ma.applicant.studentId = :studentId",
+                        MentoringApplication.class)
+                .setParameter("studentId", id)
+                .getResultList();
+
+    }
+}
