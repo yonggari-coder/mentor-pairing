f5d05ae82950c53e3aff8d6a3b2418da784edb33
yong <yonggyun0120@gmail.com>
2026-08-19 22:16:13 +0900

    feat: 멘토링프로그램 레포지토리 구현

---
+++ src/main/java/toy_project/mentor_pairing/repository/MentoringProgramRepository.java
@@ -0,0 +1,32 @@
+package toy_project.mentor_pairing.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import toy_project.mentor_pairing.domain.MatchingStatus;
+import toy_project.mentor_pairing.domain.MentoringProgram;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MentoringProgramRepository {
+
+    private final EntityManager em;
+
+    public void save(MentoringProgram mentoringProgram){
+        em.persist(mentoringProgram);
+    }
+
+    public MentoringProgram findByMatchingId(Long matchId){
+        return em.find(MentoringProgram.class, matchId);
+    }
+
+    public List<MentoringProgram> findByMenteeId(Long menteeId){    //활성화되어있는 경우에 대해서만 일단 조회
+        return em.createQuery("select mp from MentoringProgram mp where mp.mentee.studentId = :menteeId"+
+                        " and mp.mentoringStatus = :mentoringStatus", MentoringProgram.class)
+                        .setParameter("menteeId", menteeId)
+                        .setParameter("mentoringStatus", MatchingStatus.ACTIVE)
+                        .getResultList();
+    }
+
+}
