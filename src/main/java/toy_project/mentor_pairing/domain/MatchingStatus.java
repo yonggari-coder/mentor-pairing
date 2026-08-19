409598a0b3d118c6e5650b3cdd40fb5273ed07fd
yong <yonggyun0120@gmail.com>
2026-08-19 22:02:00 +0900

    refactor: MatchingStatus에 상태 추가(ENDED)

--- src/main/java/toy_project/mentor_pairing/domain/MatchingStatus.java
+++ src/main/java/toy_project/mentor_pairing/domain/MatchingStatus.java
@@ -1,5 +1,5 @@
 package toy_project.mentor_pairing.domain;
 
 public enum MatchingStatus {
-    ACTIVE, CANCELED_BY_MENTOR, CANCELED_BY_MENTEE, CANCELED_BY_ADMIN
+    ACTIVE, CANCELED_BY_MENTOR, CANCELED_BY_MENTEE, CANCELED_BY_ADMIN, ENDED
 }
