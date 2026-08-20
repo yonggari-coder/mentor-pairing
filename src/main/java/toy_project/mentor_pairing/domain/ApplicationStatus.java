f4a7fa2b61fc5086871f335ff14d37f67b239369
yong <yonggyun0120@gmail.com>
2026-08-20 14:36:37 +0900

    refactor: 신청상태 enum에 MATCHED 추가

--- src/main/java/toy_project/mentor_pairing/domain/ApplicationStatus.java
+++ src/main/java/toy_project/mentor_pairing/domain/ApplicationStatus.java
@@ -4,5 +4,6 @@ public enum ApplicationStatus {
     APPROVED,   //관리자가 승인한 상태
     PENDING,    //승인 대기 상태
     CANCELED,   //신청자가 취소한 상태
-    DISAPPROVAL //관리자가 거절한 상태
+    DISAPPROVAL, //관리자가 거절한 상태
+    MATCHED     //현재 매칭된 상태
 }
