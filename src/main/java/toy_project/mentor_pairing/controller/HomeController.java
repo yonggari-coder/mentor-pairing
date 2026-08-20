c52942f2ce5744323b0b34db2298d06554354f23
yong <yonggyun0120@gmail.com>
2026-08-20 16:18:46 +0900

    feat(화면): index.html 및 home 컨트롤러 구현

---
+++ src/main/java/toy_project/mentor_pairing/controller/HomeController.java
@@ -0,0 +1,13 @@
+package toy_project.mentor_pairing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
