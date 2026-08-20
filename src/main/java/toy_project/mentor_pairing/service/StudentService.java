74d420751c9500b18aec1df97e45b1d231870082
yong <yonggyun0120@gmail.com>
2026-08-20 16:48:29 +0900

    feat: studentService에서 전체 학생목록 조회 메서드 구현 (Transactional 어노테이션을 활용해 쓰기 메서드만 별도 설정)

--- src/main/java/toy_project/mentor_pairing/service/StudentService.java
+++ src/main/java/toy_project/mentor_pairing/service/StudentService.java
@@ -6,14 +6,21 @@ import org.springframework.transaction.annotation.Transactional;
 import toy_project.mentor_pairing.domain.Student;
 import toy_project.mentor_pairing.repository.StudentRepository;
 
+import java.util.List;
+
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
 
+    @Transactional
     public Long registerStudent(Student student){
         studentRepository.save(student);
         return student.getStudentId();
+    }
+
+    public List<Student> findStudents(){
+        return studentRepository.findAll();
     }
 }
