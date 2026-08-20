001fc9e5210205b15cf53fc88255d90276c921ca
yong <yonggyun0120@gmail.com>
2026-08-20 14:37:01 +0900

    feat: 멘토링프로그램 서비스 구현

---
+++ src/main/java/toy_project/mentor_pairing/service/MentoringProgramService.java
@@ -0,0 +1,69 @@
+package toy_project.mentor_pairing.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toy_project.mentor_pairing.domain.*;
+import toy_project.mentor_pairing.recommendation.MentorRecommendation;
+import toy_project.mentor_pairing.repository.MentoringApplicationRepository;
+import toy_project.mentor_pairing.repository.MentoringProgramRepository;
+
+import java.util.List;
+
+@Service현
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MentoringProgramService {
+    private final MentoringProgramRepository mentoringProgramRepository;
+    private final MentoringApplicationRepository mentoringApplicationRepository;
+    private final MentorRecommendationService mentorRecommendationService;
+
+    @Transactional
+    public Long match(Long menteeApplicationId, Long mentorApplicationId){
+        //1. 멘티 신청 조회
+        MentoringApplication menteeApplication = mentoringApplicationRepository.findByApplicationId(menteeApplicationId);
+        //2. 멘토 신청 조회
+        MentoringApplication mentorApplication = mentoringApplicationRepository.findByApplicationId(mentorApplicationId);
+        //3. 두 신청이 존재하는지 확인
+        if(menteeApplication == null) throw new IllegalArgumentException("멘티신청이 존재하지 않습니다.");
+        if(mentorApplication == null) throw new IllegalArgumentException("멘토신청이 존재하지 않습니다.");
+        //4. 각각 MENTEE, MENTOR 역할 확인
+        if(menteeApplication.getRole()!= MentoringRole.MENTEE) throw new IllegalStateException("신청 역할과 멘티가 일치하지 않습니다.");
+        if(mentorApplication.getRole()!= MentoringRole.MENTOR) throw new IllegalStateException("신청 역할과 멘토가 일치하지 않습니다.");
+        //5. 두 신청 모두 Pending인지 확인
+        if(menteeApplication.getStatus() != ApplicationStatus.PENDING) throw new IllegalStateException("멘토링 신청 대기상태가 아닙니다.");
+        if(mentorApplication.getStatus() != ApplicationStatus.PENDING) throw new IllegalStateException("멘토링 신청 대기상태가 아닙니다.");
+        //6. 신청 과목이 같은지 확인
+        if(mentorApplication.getSubject() != menteeApplication.getSubject()) throw new IllegalStateException("신청 과목이 서로 다릅니다.");
+        //7. 멘티 기존 Active 매칭 확인
+        Long menteeId = menteeApplication.getApplicant().getStudentId();
+        List<MentoringProgram> menteeNowActiveMentoringList = mentoringProgramRepository.findByMenteeId(menteeId);
+        for(MentoringProgram mentoringNowProgram : menteeNowActiveMentoringList){
+            if(mentoringNowProgram.getSubject().equals(menteeApplication.getSubject())) throw new IllegalStateException("해당 과목으로 멘토링 진행중입니다.");
+            if(mentoringNowProgram.getMentor().getStudentId().equals(mentorApplication.getApplicant().getStudentId())) throw new IllegalStateException("이미 신청 멘토와 멘토링 진행중입니다.");
+        }
+        //8. 현재 추천 결과에 선택 멘토가 포함되는지 확인
+        List<MentorRecommendation> mentorRecommendationList = mentorRecommendationService.recommend(menteeId, menteeApplication.getSubject());
+        boolean mentorExistInRecommendation = false;
+        for(MentorRecommendation mentorRecommendation : mentorRecommendationList){
+            if(mentorRecommendation.mentorId().equals(mentorApplication.getApplicant().getStudentId())){
+                mentorExistInRecommendation = true;
+            }
+        }
+        if(!mentorExistInRecommendation) throw new IllegalStateException("추천 멘토에 신청 멘토가 존재하지 않습니다.");
+
+        //9. 멘토링프로그램 생성 및 저장
+        MentoringProgram mentoringProgram = new MentoringProgram(
+                mentorApplication.getApplicant(),
+                menteeApplication.getApplicant(),
+                menteeApplication.getSubject(),
+                ""
+                );
+        mentoringProgramRepository.save(mentoringProgram);
+
+        //10. 멘티 신청을 MATCHED로 변경
+        menteeApplication.setStatus(ApplicationStatus.MATCHED);
+        //11. 생성된 매칭 ID 리턴
+        return mentoringProgram.getMentoringProgramId();
+    }
+}
